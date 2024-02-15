f40ea442e1c3f52d589522d6814f49c3d2961f4e
Shreya Joshi <joshishreya2502@gmail.com>
2024-02-15 23:42:34 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Projects from './components/projects/projects'
 import Footer from './components/footer/footer'
 import Intro from './components/intro/intro'
+import Skills from './components/skills/skills'
 
 const App = () => {
   return (
     <div>
       <Intro/>
+      <Skills/>
       <Projects/>
       <Footer/>
     </div>
