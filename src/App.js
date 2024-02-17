8453f4df790f20f57167ca7285e681a99651531a
Shreya Joshi <joshishreya2502@gmail.com>
2024-02-17 21:47:27 +0530

    socials

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Projects from './components/projects/projects'
 import Footer from './components/footer/footer'
 import Intro from './components/intro/intro'
 import Skills from './components/skills/skills'
-import {BrowserRouter as Main, Route, Routes} from 'react-router-dom';
 
 
 const App = () => {
