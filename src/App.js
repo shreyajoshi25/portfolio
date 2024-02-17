6e6ad98f6add7c00e549260c56d529d9194969e8
Shreya Joshi <joshishreya2502@gmail.com>
2024-02-17 21:47:07 +0530

    socials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Projects from './components/projects/projects'
 import Footer from './components/footer/footer'
 import Intro from './components/intro/intro'
 import Skills from './components/skills/skills'
+import {BrowserRouter as Main, Route, Routes} from 'react-router-dom';
+
 
 const App = () => {
   return (
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,6 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Link } from '@mui/material';
 
 const Footer = () => {
   return (
@@ -13,10 +12,10 @@ const Footer = () => {
         <div className="info">
             <span>2024 Shreya Joshi</span>
             <div className="socials">
-                <Link to="https://www.facebook.com/profile.php?id=100077572454478"><FacebookIcon className="social"/></Link>
-                <Link to="https://www.instagram.com/shreyyajoshii/"><InstagramIcon className="social"/></Link>
-                <Link to="https://www.linkedin.com/in/shreya-joshi-igdtuw/"><LinkedInIcon className="social"/></Link>
-                <Link to="https://github.com/shreyajoshi25"><GitHubIcon className="social"/></Link>
+              <a href="https://www.facebook.com/profile.php?id=100077572454478"><FacebookIcon className="social"/></a>
+              <a href="https://www.instagram.com/shreyyajoshii/"><InstagramIcon className="social"/></a>
+              <a href="https://www.linkedin.com/in/shreya-joshi-igdtuw/"><LinkedInIcon className="social"/></a>
+              <a href="https://github.com/shreyajoshi25"><GitHubIcon className="social"/></a>
             </div>
         </div>
     </div>
