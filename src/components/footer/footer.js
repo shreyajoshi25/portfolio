81a2b91db28b775e4b2d4e798a48832335e92953
Shreya Joshi <joshishreya2502@gmail.com>
2024-02-18 23:46:34 +0530

    add

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./footer.css"
-import FacebookIcon from '@mui/icons-material/Facebook';
+// import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className="info">
             <span>2024 Shreya Joshi</span>
             <div className="socials">
-              <a href="https://www.facebook.com/profile.php?id=100077572454478"><FacebookIcon className="social"/></a>
+              {/* <a href="https://www.facebook.com/profile.php?id=100077572454478"><FacebookIcon className="social"/></a> */}
               <a href="https://www.instagram.com/shreyyajoshii/"><InstagramIcon className="social"/></a>
               <a href="https://www.linkedin.com/in/shreya-joshi-igdtuw/"><LinkedInIcon className="social"/></a>
               <a href="https://github.com/shreyajoshi25"><GitHubIcon className="social"/></a>
