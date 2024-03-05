eac72343225dd53affcbc40a0d46de84e84975f7
Shreya Joshi <joshishreya2502@gmail.com>
2024-03-05 21:00:58 +0530

    content-navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,7 +3,14 @@ import "./navbar.css"
 
 const navbar = () => {
   return (
-    <div className="navbar"></div>
+    <div className="navbar">
+      <div className="left">
+        <span>About</span>
+        <span>Projects</span>
+        <span>Resume</span>
+        <span>Contact</span>
+      </div>
+    </div>
   )
 }
 
