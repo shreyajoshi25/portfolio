d10468ecfbe2f756f8fa041591d93a093cf857a3
Shreya Joshi <joshishreya2502@gmail.com>
2024-03-07 13:56:27 +0530

    add

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,8 +5,24 @@ const Contact = () => {
   return (
     <div className='contact'>
         <h1>Connect with me</h1>
-        <div className="left"></div>
-        <div className="right"></div>
+
+        <div className="sides">
+          <div className="left">
+            <span>Would love to connect with you</span>
+          </div>
+          <div className="right">
+            <form action="" className='form'>
+              <span>Name</span>
+              <input type="text" placeholder='Your Name'/>
+              <span>Email Address</span>
+              <input type="text" placeholder='Your Email Address'/>
+              <span>Message</span>
+              <input type="text" placeholder='Type in your message'/>
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
+        
     </div>
   )
 }
