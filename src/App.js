4e4bd116fad6ea834bc3ad828269c2f3e5581b4b
Shreya Joshi <joshishreya2502@gmail.com>
2024-03-21 00:18:11 +0530

    add

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Intro from './components/intro/intro'
 import Skills from './components/skills/skills'
 import Navbar from './components/navbar/navbar'
 import Contact from './components/contact/contact'
-import Exp from './components/exp/exp'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Exp/>
-      <Contact/>
       <Intro/>
       <Skills/>
       <Projects/>
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -4,9 +4,29 @@ import "./intro.css";
 const Intro = () => {
   return (
     <div className='intro'>
+      <div className="left">
         <h1>Hi, I'm Shreya Joshi</h1>
         <h2>A Software Developer</h2>
+      </div>
+
+
+      <div className='right'>
+        <div className="exp">
+          <div className="date">
+            <span>06/2023 - 07/2023</span>
+          </div>
+          <div className="detail">
+            <h4>Web Developer Intern</h4>
+            <h5>NoQS Digital Pvt. Ltd.</h5>
+            <span>worked as a web developer intern in NoQS Digital Pvt Ltd for 1.5
+months.</span>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
+
   )
 }
 
