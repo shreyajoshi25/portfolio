647e6d7f65b93aaab6e3947be051295de6e1472d
Shreya Joshi <joshishreya2502@gmail.com>
2024-03-27 09:08:11 +0530

    add

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -9,13 +9,25 @@ const Skills = () => {
         <div className="webd top" >
           <span>Web Development</span>
         </div>
+        <div className="in">
+          <div className="left-in">
+            <div className="point">
+              <span>HTML</span>
+            </div>
+          </div>
+          <div className="right-in">
+            <div className="point">
+              <span>CSS3</span>
+            </div>
+          </div>
+        </div>
         <div className="sides">
           <div className="left">
             <div className="point">
               <span>Java</span>
             </div>
             <div className="point">
-              <span>DBMS</span>
+              <span>MySQL</span>
             </div>
           </div>
           <h1>My Skills</h1>
@@ -28,6 +40,19 @@ const Skills = () => {
             </div>
           </div>
         </div>
+        <div className="in">
+          <div className="left-in">
+            <div className="point">
+              <span>Javascript</span>
+            </div>
+          </div>
+          <div className="right-in">
+            <div className="point">
+              <span>Bootstrap</span>
+            </div>
+          </div>
+        </div>
+
         <div className="bottom point">
           <span>ReactJS</span>
         </div>
