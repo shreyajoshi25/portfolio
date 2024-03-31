1ee0d98628a9096c783982fba633cfcf8f70df68
Shreya Joshi <joshishreya2502@gmail.com>
2024-03-31 23:30:24 +0530

    change

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,23 +3,26 @@ import "./projects.css"
 import cleanConnect from "../../assets/cleanConnect.png";
 import trashCollect from "../../assets/trashCollect.png";
 import social from "../../assets/social.png";
+import {Link} from "react-router-dom"
 
 const Projects = () => {
   return (
     <div className='projects'>
       <h1>My Recent Projects</h1>
       <div className="container">
-      
-      <div className="cards">
-          <img src={social} alt="" className='img'/>
-          <div className="info">
-            <div className="date">January 2024</div>
-            <div className="header">VitalLink</div>
-            <div className="details">A ReactJS-based web application designed to serve as a social media platform tailored for elders, fostering connections with friends living in proximity. The platform not only facilitates social interactions but also promotes the cultivation of new friendships, all within the context of maintaining and enhancing physical fitness.</div>
+
+        <Link to="https://shreyajoshi25.github.io/Social-media-webapp/" style={{textDecoration: 'none'}}>
+          <div className="cards">
+            <img src={social} alt="" className='img'/>
+            <div className="info">
+              <div className="date">January 2024</div>
+              <div className="header">VitalLink</div>
+              <div className="details">A ReactJS-based web application designed to serve as a social media platform tailored for elders, fostering connections with friends living in proximity. The platform not only facilitates social interactions but also promotes the cultivation of new friendships, all within the context of maintaining and enhancing physical fitness.</div>
+            </div>
           </div>
-        </div>
+        </Link>
 
-
+        <Link to="https://anjalisilawat.github.io/HashItOut/" style={{textDecoration: 'none'}}>
         <div className="cards">
           <img src={cleanConnect} alt="" className='img'/>
           <div className="info">
@@ -28,8 +31,9 @@ const Projects = () => {
             <div className="details">A application which could serve as a bridge between the locals and the municipality.</div>
           </div>
         </div>
+        </Link>
 
-        
+        <Link to="https://shreyajoshi25.github.io/game/" style={{textDecoration: 'none'}}>
         <div className="cards">
           <img src={trashCollect} alt="" className='img'/>
           <div className="info">
@@ -38,6 +42,8 @@ const Projects = () => {
             <div className="details">Developed and launched a dynamic JavaScript web game, showcasing seamless gameplay, captivating visuals, and engaging challenges—all accessible directly through the browser.</div>
           </div>
         </div>
+        </Link>
+
       </div>
     </div>
   )
