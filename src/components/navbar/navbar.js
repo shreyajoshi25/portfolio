2b3d147a926b83cd354457e93b892b5d2f67eef6
Shreya Joshi <joshishreya2502@gmail.com>
2024-04-01 10:48:10 +0530

    add

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./navbar.css"
+import {Link} from "react-router-dom";
 
 const navbar = () => {
   return (
@@ -10,10 +11,18 @@ const navbar = () => {
         <span>Joshi</span>
       </div>
       <div className="right">
+        <Link to='/' style={{textDecoration: 'none', color:'black'}}>
         <span>Home</span>
+        </Link>
+        <Link to='projects' style={{textDecoration: 'none', color:'black'}}>
         <span>Projects</span>
+        </Link>
+        <Link to='' style={{textDecoration: 'none', color:'black'}}>
         <span>Resume</span>
+        </Link>
+        <Link to='contact' style={{textDecoration: 'none', color:'black'}}>
         <span>Contact</span>
+        </Link>
       </div>
     </div>
   )
