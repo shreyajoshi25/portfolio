cb2a552aa579be41dab4db98158ee4a9b9de2d6d
Shreya Joshi <joshishreya2502@gmail.com>
2024-04-08 21:52:40 +0530

    add

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -11,64 +11,90 @@ const Resume = () => {
       <div className='right'>
         <h4 className='top'>WORK EXPERIENCE AND MEMBERSHIPS</h4>
         <h2>My Experience</h2>
-        <div className="exp">
-          <div className="date">
-            <span>02/2024 - Present</span>
+        <div className="point">
+
+          <div className="bullet">
+            <div className="purple"></div>
           </div>
-          <div className="detail">
-            <h4>CodeSolstice'24 Mentor</h4>
-            <h5>Protege, IGDTUW</h5>
-            <span>Started as a mentor for a CodeSolstice Mentorship Cohort at Protege, IGDTUW</span>
+          <div className="exp">
+            <div className="detail">
+              <div className="head">
+                <h4>Protege, IGDTUW</h4>
+                <span>(02/2024 - Present)</span>
+              </div>
+              <span>Started as a mentor for a CodeSolstice Mentorship Cohort at Protege, IGDTUW</span>
+              <button>CodeSolstice'24 Mentor</button>
+            </div>
           </div>
         </div>
 
-        <div className="exp">
-          <div className="date">
-            <span>05/2023 - Present</span>
+        <div className="point">
+          <div className="bullet">
+            <div className="purple"></div>
           </div>
-          <div className="detail">
-            <h4>Social Media Manager and Web Development Team</h4>
-            <h5>Microsoft Students Chapter, IGDTUW</h5>
-            <span>Working as a member of the web development team and also as the Social Media Manager of MSC.</span>
-          </div>
-        </div>
 
-
-        <div className="exp">
-          <div className="date">
-            <span>01/2022 - Present</span>
-          </div>
-          <div className="detail">
-            <h4>Event Management CORE</h4>
-            <h5>Taarangana, IGDTUW</h5>
-            <span>Currently serving as a core member of the Event Management team for Taarangana's 2023-2024 session. Previously held roles as Coordinator and Head Coordinator for IGDTUW's cultural fest in 2022, followed by promotion to the Associate position in 2023.</span>
-          </div>
-        </div>
-
-
-        <div className="exp">
-          <div className="date">
-            <span>06/2023 - 07/2023</span>
-          </div>
-          <div className="detail">
-            <h4>Web Developer Intern</h4>
-            <h5>NoQS Digital Pvt. Ltd.</h5>
-            <span>worked as a web developer intern in NoQS Digital Pvt Ltd for 1.5 months.</span>
+          <div className="exp">
+            <div className="detail">
+              <div className="head">
+                <h4>Microsoft Students Chapter, IGDTUW</h4>
+                <span>(05/2023 - Present)</span>
+              </div>
+              <span>Working as a member of the web development team and also as the Social Media Manager of MSC.</span>
+              <button>Social Media Manager and Web Development Team</button>
+            </div>
           </div>
         </div>
 
 
-        <div className="exp">
-          <div className="date">
-            <span>09/2022 - 12/2023</span>
+        <div className="point">
+          <div className="bullet">
+            <div className="purple"></div>
           </div>
-          <div className="detail">
-            <h4>PR HEAD</h4>
-            <h5>Hypnotics, IGDTUW</h5>
-            <span>PR head of the Western Dance Society of Indira Gandhi Delhi Technical University for Women.</span>
+
+          <div className="exp">
+            <div className="detail">
+              <div className="head">
+                <h4>Taarangana, IGDTUW</h4>
+                <span>(01/2022 - Present)</span>
+              </div>
+              <span>Currently serving as a core member of the Event Management team for Taarangana's 2023-2024 session. Previously held roles as Coordinator and Head Coordinator for IGDTUW's cultural fest in 2022, followed by promotion to the Associate position in 2023.</span>
+              <button>Event Management CORE</button>
+            </div>
           </div>
         </div>
-        
+
+        <div className="point">
+          <div className="bullet">
+            <div className="purple"></div>
+          </div>
+
+          <div className="exp">
+            <div className="detail">
+              <div className="head">
+                <h4>NoQS Digital Pvt. Ltd.</h4>
+                <span>(06/2023 - 07/2023)</span>
+              </div>
+              <span>worked as a web developer intern in NoQS Digital Pvt Ltd for 1.5 months.</span>
+              <button>Web Developer Intern</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="point">
+          <div className="bullet">
+            <div className="purple"></div>
+          </div>
+          <div className="exp">
+            <div className="detail">
+              <div className="head">
+              <h4>Hypnotics, IGDTUW</h4>
+                <span>(09/2022 - 12/2023)</span>
+              </div>
+              <span>PR head of the Western Dance Society of Indira Gandhi Delhi Technical University for Women.</span>
+              <button>PR HEAD</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
