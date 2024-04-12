467cca9458b146f909b128fe2253703d630f4ed7
Shreya Joshi <joshishreya2502@gmail.com>
2024-04-12 21:58:30 +0530

    add

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./contact.css"
+import { useForm, ValidationError } from '@formspree/react';
 import intro_photo from "../../assets/intro_photo.png";
 
-const Contact = () => {
+function Contact() {
+  const [state, handleSubmit] = useForm("xnqeoljn");
+  if (state.succeeded) {
+      return <p>Thanks for contacting!</p>;
+  }
+
   return (
     <div className='contact'>
         <h1>Connect with me</h1>
@@ -17,14 +23,14 @@ const Contact = () => {
             {/* <img src={intro_photo} alt="" /> */}
           </div>
           <div className="right">
-            <form action="" className='form'>
+            <form action="https://formspree.io/f/xnqeoljn" method="POST" className='form'>
               <span>Name</span>
-              <input type="text" className='input'/>
+              <input type="text" name="Name" className='input' required/>
               <span>Email Address</span>
-              <input type="text" className='input'/>
+              <input type="text" name="Email" className='input' required/>
               <span>Message</span>
-              <input type="text" className='input message'/>
-              <button>Send</button>
+              <input type="text" name="Message" className='input message' required/>
+              <button type='submit' value='send'>Send</button>
             </form>
           </div>
         </div>
@@ -33,4 +39,9 @@ const Contact = () => {
   )
 }
 
+function App(){
+  return (
+    <Contact/>
+  )
+}
 export default Contact
