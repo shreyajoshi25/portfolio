a808369472896816b78d4aaa1d3452203eca2829
Shreya Joshi <joshishreya2502@gmail.com>
2024-04-17 11:47:23 +0530

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/navbar/navbar'
 import Contact from './components/contact/contact'
 import Resume from './components/resume/resume'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import './App.css'
 
 const App = () => {
   return (
