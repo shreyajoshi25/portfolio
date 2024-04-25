4d8c308adf185e55b29234c61257d1a9cb63a026
Shreya Joshi <joshishreya2502@gmail.com>
2024-04-25 23:04:38 +0530

    add

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,48 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./navbar.css"
+// import styles from "./navbar.css"
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import {Link} from "react-router-dom";
 import logo from "../../assets/logo.png"
+import menuIcon from "../../assets/menuIcon.png"
+import closeIcon from "../../assets/closeIcon.png"
 
-const navbar = () => {
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    console.log("Before toggle:", menuOpen);
+    setMenuOpen(prevMenuOpen => !prevMenuOpen);
+  };
+
+  console.log("Rendered with menuOpen:", menuOpen);
+
   return (
     <div className="navbar">
       <div className="left">
         <img src={logo} alt="" />
       </div>
-      <div className="middle">
-        <Link to='/' style={{textDecoration: 'none', color:'black'}}>
-        <span>Home</span>
-        </Link>
-        <Link to='projects' style={{textDecoration: 'none', color:'black'}}>
-        <span>Projects</span>
-        </Link>
-        <Link to='resume' style={{textDecoration: 'none', color:'black'}}>
-        <span>Resume</span>
-        </Link>
-        <Link to='contact' style={{textDecoration: 'none', color:'black'}}>
-        <span>Contact</span>
-        </Link>
+
+      <div className="menu">
+        <img className="btn" src={menuOpen ? (closeIcon) : (menuIcon)} alt="" onClick={handleMenuToggle}/>
+        <ul className="show">
+          <Link to='/' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Home</li>
+          </Link>
+          <Link to='projects' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Projects</li>
+          </Link>
+          <Link to='resume' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Resume</li>
+          </Link>
+          <Link to='contact' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Contact</li>
+          </Link>
+        </ul>
+
+        <ul className="hide" style={{ display: menuOpen ? 'flex' : 'none' }} onClick={() => setMenuOpen(false)}>
+          <Link to='/' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Home</li>
+          </Link>
+          <Link to='projects' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Projects</li>
+          </Link>
+          <Link to='resume' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Resume</li>
+          </Link>
+          <Link to='contact' style={{ textDecoration: 'none', color: 'black' }}>
+            <li>Contact</li>
+          </Link>
+        </ul>
       </div>
 
-      <a href="mailto:joshishreya2502@gmail.com" ><div className="right">
+      <a href="mailto:joshishreya2502@gmail.com" className='mail' ><div className="right">
        <EmailOutlinedIcon className='mail'></EmailOutlinedIcon>
         <span>joshishreya2502@gmail.com</span>
       </div> </a>
 
-      <div className="hide">
-        <li>
-          <ul>Home</ul>
-          <ul>Projects</ul>
-          <ul>Resume</ul>
-          <ul>Contact</ul>
-          <hr />
-          <ul><a href="mailto:joshishreya2502@gmail.com" ><div className="email"><EmailOutlinedIcon className='mail'></EmailOutlinedIcon><span>joshishreya2502@gmail.com</span></div></a></ul>
-        </li>
-
-      </div>
+      
     </div>
   )
 }
 
-export default navbar
+export default Navbar
